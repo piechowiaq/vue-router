8b84f19cd55c84adbfa51101b51d8d5e64025f6a
Bartosz Piechowiak <piechowiaq@gmail.com>
2023-06-20 20:23:38 +0200

    Dynamic car data

---
+++ src/views/CarView.vue
@@ -0,0 +1,23 @@
+<script setup>
+
+import {useRoute} from "vue-router"
+import cars from "../data/cars.json"
+
+const route = useRoute()
+
+const car = cars.find(car => car.id === parseInt(route.params.id))
+
+
+</script>
+
+<template>
+  <div>
+    <h1>Car View</h1>
+    <p>{{car.name}}</p>
+  </div>
+</template>
+
+
+<style scoped>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,34 @@
-<template>
-<div>
-  <h1>Home View</h1>
-</div>
-</template>
+<script setup>
 
-<script>
-export default {
-  name: "HomeView"
-}
+import cars from "../data/cars.json"
+import {RouterLink} from "vue-router";
+
+
 </script>
 
+<template>
+  <div>
+    <h1>Home View</h1>
+    <div class="cars">
+      <RouterLink v-for="car in cars" :key="car.id" :to="`/cars/${car.id}`">
+        {{car.name}}
+      </RouterLink>
+    </div>
+  </div>
+</template>
+
 <style scoped>
+
+.cars {
+  display: flex;
+}
+
+a {
+  text-decoration: none;
+  color: #2c3e50;
+  width: 100%;
+  box-shadow: 1px 1px 10px black;
+  margin: 10px;
+}
 
 </style>
